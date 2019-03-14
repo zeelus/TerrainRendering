a90d61575843fdae8ee31db67ca8f7f882db015c
Gilbert Gwizdala <zeelus.nano@gmail.com>
2019-03-14 21:31:58 +0100

    Add imput code

--- Source/Engine/Input.cpp
+++ Source/Engine/Input.cpp
@@ -1,5 +1,30 @@
 //
-// Created by zeelu on 14.03.2019.
+// Created by Gilbert Gwizdala on 14.03.2019.
 //
 
 #include "Input.h"
+
+Input* Input::instance = nullptr;
+
+Input *Input::getInstance() {
+    if(!instance) {
+        instance = new Input();
+    }
+
+    return instance;
+}
+
+void Input::key_callback(GLFWwindow *window, int key, int scancode, int action, int mods) {
+    for(auto& func: functions) {
+        func(key, scancode, action, mods);
+    }
+}
+
+void Input::key_callback_static(GLFWwindow *window, int key, int scancode, int action, int mods) {
+    auto inputSystem = reinterpret_cast<Input*>(glfwGetWindowUserPointer(window));
+    inputSystem->key_callback(window, key, scancode, action, mods);
+}
+
+void Input::addFunction(const std::function<void(int, int, int, int)> function) {
+    this->functions.push_back(function);
+}
--- Source/Engine/Input.h
+++ Source/Engine/Input.h
@@ -1,12 +1,28 @@
 //
-// Created by zeelu on 14.03.2019.
+// Created by Gilbert Gwizdala on 14.03.2019.
 //
 
 #ifndef TERRENRENDERING_INPUT_H
 #define TERRENRENDERING_INPUT_H
+#include "libs.h"
 
+#include <vector>
+#include <functional>
 
 class Input {
+
+    static Input* instance;
+
+    std::vector<std::function<void(int, int, int, int)>> functions;
+
+public:
+    static Input* getInstance();
+
+    void key_callback(GLFWwindow *window, int key, int scancode, int action, int mods);
+
+    static void key_callback_static(GLFWwindow *window, int key, int scancode, int action, int mods);
+
+    void addFunction(const std::function<void(int, int, int, int)> function);
 
 };
 
