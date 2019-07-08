ec2d8ed56bf84602686032abb68fe0c89627c5de
Gilbert Gwizdała <zeelus_nano@gmail.com>
2019-07-08 20:40:54 +0200

    Add statistic class

---
+++ Source/Engine/Statistic.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Gilbert Gwizdala on 2019-07-08.
+//
+
+#include "Statistic.h"
---
+++ Source/Engine/Statistic.h
@@ -0,0 +1,14 @@
+//
+// Created by Gilbert Gwizdala on 2019-07-08.
+//
+
+#ifndef TERRENRENDERING_STATISTIC_H
+#define TERRENRENDERING_STATISTIC_H
+
+
+class Statistic {
+
+};
+
+
+#endif //TERRENRENDERING_STATISTIC_H
